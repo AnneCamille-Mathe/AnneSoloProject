7975678492565013eec6aa6b00dd4f0fa91307f7
Annek <anne.camile.mathe@gmail.com>
2019-07-19 17:13:21 +0200

    SpawnSucced

---
+++ unity_stage/Assets/Script/ScriptSpawn.cs
@@ -0,0 +1,22 @@
+﻿///////// Script Spawn Player pour Udemy //////////////////
+///////////////////////////////////////////////////////////
+////////  Instructeur Parein Jean-philippe ////////////////
+///////////////////////////////////////////////////////////
+
+using UnityEngine;
+using System.Collections;
+
+public class ScriptSpawn : MonoBehaviour {
+    
+   	void OnTriggerEnter (Collider Other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if(Other.gameObject.name== "FPSController")
+        {
+            Other.gameObject.GetComponent<CharacterController>().enabled = false;
+            Other.gameObject.transform.position = GameObject.Find("SpawnPoint").transform.position;
+            Other.gameObject.GetComponent<CharacterController>().enabled = true;
+
+        }	
+	}		
+}
