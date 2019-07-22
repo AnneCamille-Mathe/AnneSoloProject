0cf8d7fe23cb7e5955ac6ef29c5ea2d63a48083d
Annek <anne.camile.mathe@gmail.com>
2019-07-22 10:53:51 +0200

    FallDownSucced

--- unity_stage/Assets/Script/ScriptFallDown.cs
+++ unity_stage/Assets/Script/ScriptFallDown.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class PlateformFallScript : MonoBehaviour {
+public class ScriptFallDown : MonoBehaviour {
 
     //Déclarations des Variables
 
