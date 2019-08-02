33e3edf26cde56afba8a5d2e6f69d1f94c18688f
Annek <anne.camile.mathe@gmail.com>
2019-08-02 17:19:21 +0200

    BubbleVROkToDoBugPopUp

---
+++ unity_stage/Assets/Script/Clef_escape/ClefVRScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClefVRScript : MonoBehaviour
+{
+    //Variables
+    public bool GetKeyVR = false;
+    public GameObject PanelText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetKeyVR = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            this.GetKeyVR = true;
+            StartCoroutine(this.GetKey());
+            GameObject.Find("BubbleVR/key").SetActive(false);
+            GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score += 5;
+            GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score_Info.text = "Score : " + GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score;
+            //TODO : voir pourquoi pas effacement
+        }
+    }
+    
+    IEnumerator GetKey()
+    {
+        print("OK REALISEE");
+        yield return new WaitForSeconds(4f);
+        PanelText.SetActive(false);
+    }
+}
---
+++ unity_stage/Assets/Script/Clef_escape/DoorVRScript.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorVRScript : MonoBehaviour
+{
+    //Variables
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.anim.SetTrigger("OpenDoor");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.anim.enabled = true;
+    }
+
+    void pauseAnimationEvent()
+    {
+        this.anim.enabled = false;
+    }
+}
