bdb551c652f9e58cd659be646ce8334572cb9c70
Annek <anne.camile.mathe@gmail.com>
2019-08-06 16:39:12 +0200

    CheminAuSol

---
+++ unity_stage/Assets/Script/Fleches/Fleche4Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fleche4Script : MonoBehaviour
+{
+    //Variables
+    public bool code_trouve;
+    public GameObject fleche4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.code_trouve = GameObject.Find("Canvas_empiler/TextManager").GetComponent<CombinaisonScript>().trouve;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.code_trouve = GameObject.Find("Canvas_empiler/TextManager").GetComponent<CombinaisonScript>().trouve;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (code_trouve && other.gameObject.tag == "Player")
+        {
+            Debug.Log("PUTAIN DE MERDE");
+            this.fleche4.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (GameObject.Find("BubbleVR/key") == null)
+        {
+            this.fleche4.SetActive(false);
+        }
+    }
+}
---
+++ unity_stage/Assets/Script/Fleches/Fleche5Script.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fleche5Script : MonoBehaviour
+{
+    //Variables
+    public GameObject fleche5;
+    public bool code_trouve;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.code_trouve = GameObject.Find("Canvas_empiler/TextManager").GetComponent<CombinaisonScript>().trouve;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.code_trouve = GameObject.Find("Canvas_empiler/TextManager").GetComponent<CombinaisonScript>().trouve;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && this.code_trouve)
+        {
+            this.fleche5.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (GameObject.Find("BubbleVR/key") == null)
+        {
+            this.fleche5.SetActive(false);
+        }
+    }
+}
