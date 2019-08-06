6861c2f0ae03b62645491f0a03b3f06a2ba21dce
Annek <anne.camile.mathe@gmail.com>
2019-08-06 10:52:09 +0200

    ModelisationDebutCodeSalleSortie

---
+++ unity_stage/Assets/Script/SortieRoom/DoorSortie3Script.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSortie3Script : MonoBehaviour
+{
+    
+    //Variables
+    private Animator anim;
+    //TODO - MODIFIER CETTE VARIABLE !
+    public bool coffreTrouve = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.coffreTrouve)
+        {
+            this.anim.SetTrigger("OpenDoor");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.anim.enabled = true;
+    }
+
+    void PauseAnimationEvent()
+    {
+        this.anim.enabled = false;
+    }
+}
