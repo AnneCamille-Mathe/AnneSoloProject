aadcc201fcf7488fb9ef2bad7e75f000c1bbed59
Annek <anne.camile.mathe@gmail.com>
2019-08-08 11:50:56 +0200

    CoffreClefOkToDoSortie

---
+++ unity_stage/Assets/Script/CodageFinNiveau/GetKeyScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetKeyScript : MonoBehaviour
+{
+    //Variables
+    public GameObject key;
+    public GameObject panelText;
+    //sinon incrémentation du score trop rapide
+    private int count = 0;
+    //On regarde si la clef a été prise
+    public bool ClefOuverture = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //sinon incrémentation du score à chaque entrée
+            if (this.count == 0)
+            {
+                GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score += 10;
+                GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score_Info.text = "Score : " + GameObject.Find("SpawnFPSController/ZoneSpawn").GetComponent<LifeScript>().Score;
+                this.ClefOuverture = true;
+            }
+
+            this.count += 1;
+            this.key.SetActive(false);
+            this.panelText.SetActive(true);
+            StartCoroutine(this.WaitPanel());
+        }
+    }
+
+    //On attend avant de désactiver le panel
+    IEnumerator WaitPanel()
+    {
+        yield return new WaitForSeconds(3f);
+        this.panelText.SetActive(false);
+    }
+}
---
+++ unity_stage/Assets/Script/CodageFinNiveau/OuvertureCoffreScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OuvertureCoffreScript : MonoBehaviour
+{
+    // Variables
+    Animator anim;
+    public GameObject key;
+
+    private int eviteBug = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        this.anim.SetTrigger("OuvrirCoffre");
+        //Eviter l'erreur lorsque clef disparue
+        if (this.eviteBug == 1)
+        {
+            StartCoroutine(this.WaitKey());
+            this.eviteBug = 2;
+        }
+    }
+
+    void PauseAnimationEvent()
+    {
+        this.anim.enabled = false;
+    }
+
+    IEnumerator WaitKey()
+    {
+        yield return new WaitForSeconds(1f);
+        this.key.SetActive(true);
+    }
+}
