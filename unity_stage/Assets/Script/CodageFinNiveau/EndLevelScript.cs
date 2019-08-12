207676b197e7632f5c5213bd0e4ce9e9e9a0c3df
Annek <anne.camile.mathe@gmail.com>
2019-08-12 11:53:34 +0200

    ChargementNiveau2ToDoModifN2

---
+++ unity_stage/Assets/Script/CodageFinNiveau/EndLevelScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using  UnityEngine.SceneManagement;
+
+public class EndLevelScript : MonoBehaviour
+{
+    
+    //Variables
+    public string LevelToLoad;
+    public GameObject PanelText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(this.LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        this.PanelText.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(this.LevelToLoad);
+    }
+}
