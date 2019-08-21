5c42bbc004fe3aa58e25ab0e8c86cbb854b9bec4
Annek <anne.camile.mathe@gmail.com>
2019-08-21 12:31:22 +0200

    DebutShootToContinue

---
+++ unity_stage/Assets/Script/Mini jeux projets/SEngager/EnemiesScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class EnemiesScript : MonoBehaviour
+{
+    //Variables
+    //public Sprite death;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
